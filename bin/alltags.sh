574d64f5bf4c993f6f54c2de25ed625b712a2de3
Elise Hein <elisehein@gmail.com>
2016-03-28 20:43:55 +0100

    Improved tag script

--- bin/alltags.sh
+++ bin/alltags.sh
@@ -6,8 +6,12 @@ for i in _posts/*
 do
   if test -f "$i" 
   then
-    alltags="$alltags `tail -n +2 $i | awk '/---/ {exit} {print}' | shyaml get-value tags`"
+    posttags=$(tail -n +2 $i |               `# remove first line`\
+               awk "/---/ {exit} {print}" |  `# display up to ---`\
+               shyaml get-value tags |       `# extract tags from remaining YAML`\
+               cut -d ' ' -f 2)              `# remove dashes`
+    alltags="$alltags $posttags"
   fi
 done
 
-echo $alltags | tr - "\n" | tail -n +2 | sort | uniq -c | sort -r
+echo $alltags | tr ' ' '\n' | sort | uniq -c | sort -r
