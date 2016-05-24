abae54e27332ab3e13315b7e26696545c643d09a
Elise Hein <elisehein@gmail.com>
2016-05-24 20:21:15 +0100

    More restructuring and tweaking. Use smart underlines for links.

---
+++ _drafts/2016-05-22-tufte-latex.md
@@ -0,0 +1,2 @@
+Write about the development and use of the tufte latex library for my uni
+work.
--- javascript/smallcapsdictionary.js
+++ javascript/smallcapsdictionary.js
@@ -4,14 +4,18 @@
     "YAML",
     "UX",
     "RSI",
-    "NLP"
+    "NLP",
+    "PDF",
+    "UCL",
+    "UCLIC",
+    "CHI"
   ];
 
   var articleContainer = document.getElementsByClassName("single-post__body")[0];
 
   dictionary.forEach(function (word) {
-    var regexp = new RegExp("(" + word + ")", "g"),
-        replacement = "<span class='fs-sc'>$1</span>";
+    var regexp = new RegExp("(" + word + ")([^a-zA-Z])", "g"),
+        replacement = "<span class='fs-sc'>$1</span>$2";
     articleContainer.innerHTML = articleContainer.innerHTML.replace(regexp, replacement);
   });
 })();
